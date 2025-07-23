2c51a124d7b2c63ca0716dc01289c69056deb37b
Ayush Ranjan <knight112619@gmail.com>
2025-07-23 13:48:39 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-app.use(cors({ origin: 'https://roaring-tanuki-ba40ef.netlify.app/' }));
+
 
 const PDFDocument = require('pdfkit');
 const fs = require('fs');
@@ -11,6 +11,7 @@ const { Patient } = require('./models');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors({ origin: 'https://roaring-tanuki-ba40ef.netlify.app/' }));
 
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
